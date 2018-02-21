d19df43198fba0da3b3e0f55a60e865d2dc2a91b
Sataapon <mr.sataapon@gmail.com>
2018-02-22 00:49:54 +0700

    User story more appropriate, functional test click choice and submit, add poll application, django test client, render html

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
-# MA-food-poll
+Aroyjung
 
 Project in Software Development Practice II class
-Create food-poll web application, using Django webframework and Test Driven Development process
 
-reference
+Create Foods-poll web application, using Django web framework and Test Driven Development process
+
+Reference
+
 https://docs.djangoproject.com/en/2.0/intro/ # Django tutorial
+
 https://www.obeythetestinggoat.com/pages/book.html#toc # Test Driven Development with Python 2nd Edition
 
--- functional_tests.py
+++ functional_tests.py
@@ -10,28 +10,44 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_poll_vote_it_and_see_result(self):
-        # While Pun eat food at new restaurant
+        # While Pun eat "Gang curry" and "Keghuay" at new restaurant
         # She see sign Website about food poll 
         # When She finish eat food, She go to check out its homepage 
         self.browser.get('http://localhost:8000')
 
         # She see poll in title and header
         self.assertIn('poll', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('poll', header_text)
 
-        # She select food and beverage pictures
+        ## She see food pictures with average score and number of vote
+        # She select "Gang curry" picture
+        select_food = self.browser.find_element_by_id('Gang curry')
+        select_food.click()
+
+        # She select "Keghuay" picture
+        select_beverage = self.browser.find_element_by_id('Keghuay')
+        select_beverage.click()
 
         # When she click Vote button, the page reload,
+        submit = self.browser.find_element_by_id('Submit')
+        submit.click()
+
+        self.fail('Finish the test!')
         # and now the new page appear
 
-        # She see pictures that she select with text box and score button
+        # She see only "Gang curry" and "Keghuay" picture with vote button and text box
 
-        # She comment text box, select score button
+        # She type "Very delicious" in text box and select "5" at vote buttun of "Gang curry" picture
+
+        # She type "Sweet too much" in text box and select "4" at vote button of "Keghuay" picture 
 
         # When she click Send button, the page reload,
         # and now the new page appear
 
-        # She see picture with average score and number of votes
+        # She see "Gang curry" with text "You vote: 5 score, comment: Very delicious"
+
+        # She see "Keghuay" picture text "You vote: 4 score, comment: Sweet too much"
 
         # Satisfied, she paid and walked out of the restaurant
 
