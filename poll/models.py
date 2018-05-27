3d69c59087daba11e624b4fd693e578b21684e95
sataapon <mr.sataapon@gmail.com>
2018-05-28 00:47:01 +0700

    Edited models and add aboutpage

--- poll/models.py
+++ poll/models.py
@@ -1,16 +1,20 @@
 from django.db import models
 
 class Menu(models.Model):
-    name = models.CharField(default=None, max_length=50)
-    Type = models.CharField(default=None, max_length=50)
+    name = models.CharField(default = None, max_length = 50)
+    Type = models.CharField(default = None, max_length = 50)
 
     def __str__(self):
         return self.name
 
 class User(models.Model):
-    menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
-    comment = models.TextField(default=None)
-    score = models.IntegerField(default=None)
+    name = models.CharField(default = None, max_length = 50)
 
     def __str__(self):
-        return self.menu
+        return self.name
+
+class CommentScore(models.Model):
+    menu = models.ForeignKey(Menu, on_delete = models.CASCADE)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    comment = models.TextField(null = True)
+    score = models.IntegerField(null = True)
